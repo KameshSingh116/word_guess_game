4647b68d77bb7c96ed86b4a9f7f828fa810ce3a3
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-25 16:31:53 +0530

    changes

--- main.py
+++ main.py
@@ -55,24 +55,31 @@ elif word=='programming':
 
 elif word=='python':
         print(random.choice(t))
-         
-while(turns>0):
-    guess=input("Your guess:")
-    if word==guess:
-        print(f"You guessed it right {name}")
-        break
+
+choice='y'
+while(choice=='y'):
+        while(turns>0):
+          guess=input("Your guess:")
+          if word==guess:
+             print(f"You guessed it right {name}")
+             break
     
-    elif(word!="guess"):
-        print(f"Try again you are left  with {turns-1}")
-        turns=turns-1
-        if(turns==0):
-            print("Awwwwww! no tries left!")
+          elif(word!="guess"):
+             print(f"Try again you are left  with {turns-1}")
+             turns=turns-1
+             if(turns==0):
+                print("Awwwwww! no tries left!")
 
-    else:
-        print("Invalid input buddy!")
-        turns=-1
+          else:
+             print("Invalid input buddy!")
+             turns=-1
 
-    if(turns==0):
-        print("You weren't able to guess the word!")
-        print(f"The word is {word}")
-
+          if(turns==0):
+             print("You weren't able to guess the word!")
+             print(f"The word is {word}")
+      
+        choice=input("Do you want to continue:(y/n):")
+        if(choice=='y'):
+          continue
+        else:
+          break
