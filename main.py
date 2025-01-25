708c114172ad77db779cf30d2dcf37119f35aa3d
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-25 15:34:26 +0530

    initializations

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 #Radhe Radhe
 import random
+name=input("What should i call you?:")
+print(f"Let's Go {name}")
+
+words = ['rainbow', 'computer', 'science', 'programming',
+         'python', 'mathematics', 'player', 'condition',
+         'reverse', 'water', 'board', 'geeks']
+
+word=random.choice(words)
+
+print(f"{name} start guessing")
+
+guesses=''
+turns=10
