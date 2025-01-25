9e22f7cf91e7c47c7a4f43a3aac97d9da055a13c
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-25 16:06:53 +0530

    almost there

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import random
 name=input("What should i call you?:")
 print(f"Let's Go {name}")
 
-words = ['rainbow', 'computer', 'science', 'programming',
-         'python', 'mathematics', 'player', 'condition',
-         'reverse', 'water', 'board', 'geeks']
+words = ['rainbow', 'computer', 'water', 'programming',
+         'python']
 
 word=random.choice(words)
 
@@ -13,3 +12,28 @@ print(f"{name} start guessing")
 
 guess=''
 turns=3
+size=len(word)
+print("...HINT...")
+hint=print(f"The word has {size} characters!")
+
+while(turns>0):
+    guess=input("Your guess:")
+
+    if word==guess:
+        print(f"You guessed it right {name}")
+        break
+    
+    elif(word!="guess"):
+        print(f"Try again you are left  with {turns-1}")
+        turns=turns-1
+        if(turns==0):
+            print("Awwwwww! no tries left!")
+
+    else:
+        print("Invalid input buddy!")
+        turns=-1
+
+    if(turns==0):
+        print("You weren't able to guess the word!")
+        print(f"The word is {word}")
+
